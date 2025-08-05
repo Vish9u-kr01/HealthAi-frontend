2f8cc98a771564356626f576ca67ccd616f78154
Vishnu kr <vish9u1424@gmail.com>
2025-08-05 21:29:15 +0530

    Delete frontend directory

--- frontend/app/actions/form.js
+++
@@ -1,5 +0,0 @@
-"use server"
-import fs from "fs/promises"
-export const submitAction = async (e) => {
-    await fs.writeFile("feedback.txt", `Name:${e.get("name")} , Email:${e.get("email")} and Message:${e.get("message")}\n`,{flag:"a"}); 
-  }
