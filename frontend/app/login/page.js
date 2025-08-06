0565966e19ef83d4fb3d608496fa991a0902032b
Vishnu kr <vish9u1424@gmail.com>
2025-08-06 20:07:19 +0530

    Update page.js

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -14,7 +14,7 @@ const LoginForm = () => {
 
   const onSubmit = async (data) => {
     try {
-      const res = await fetch('http://localhost:3000/login', {
+      const res = await fetch('https://healthai-backend-4.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
@@ -309,6 +309,7 @@ export default LoginForm;
 // };
 
 // export default SignupForm;
+
 
 
 
